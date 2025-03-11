23ffe41e499be037c3ada8a9511e2451a2585d44
Divyamani-1722 <gorrumuchudivyamani@gmail.com>
2025-03-11 20:44:27 +0530

    DFD commit

--- retail-hub-app/src/App.js
+++ retail-hub-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SignUpForm from './components/SignUpForm';
+import SignUpForm from './components/SignUpForm.js';
 
 const App = () => (
   <Router>
